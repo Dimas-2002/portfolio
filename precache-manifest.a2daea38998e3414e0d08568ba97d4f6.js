49db8587ac489c9edb9cb46f4f25a5d3e312a3ef
Dimas Heryunanto <dimasheryunanto22@gmail.com>
2020-02-23 20:44:02 +0700

    Updates

---
+++ precache-manifest.a2daea38998e3414e0d08568ba97d4f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5856b97775eed96b1698d48ec7acfb92",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7cf3234d699b7d96d1e0",
+    "url": "/portfolio/static/css/main.7bcc1bc2.chunk.css"
+  },
+  {
+    "revision": "a99e4a1fb7f4270c3b21",
+    "url": "/portfolio/static/js/2.4e92ce17.chunk.js"
+  },
+  {
+    "revision": "7cf3234d699b7d96d1e0",
+    "url": "/portfolio/static/js/main.b77271e7.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
